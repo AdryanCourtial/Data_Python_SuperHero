5b947ce76a1d68c788a02b5d8c105445c7f3a757
AdryanCourtial <adryan.courtial@ynov.com>
2024-04-15 17:56:03 +0200

    Content 2 Faut fix des beugs

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,10 @@
 from dash import register_page, callback, dcc,  Output, Input, State
 from dash import html
+from dash import Dash, html, dcc, callback, Output, Input, State
 from graph.film_graph import update_film_graph
+from graph.comparison_graph import update_main_graph
+from graph.critic_graph import update_critic_graph
+from graph.pie_chart import create_pie_chart
 import app
 
 register_page(__name__, path='/')
@@ -24,10 +28,11 @@ layout = html.Div(
             className="content",
             children=[
                 html.Div(
-                    className="content-1",
                     children = [
                         html.H2("Recette Généré dans le monde entier (Brute)"),
-                        html.Div([
+                        html.Div(
+                            id="content-1",
+                            children= [
                             html.H1(children='Analyse des données', className='text-center mb-4'),
                             html.Div([
                                 dcc.Dropdown(
@@ -44,6 +49,17 @@ layout = html.Div(
                         html.P("""Je suis La description de fou mec c'est incroyable
                                 comment baptiste est beau et genre personne le remarque""")
                     ]
+                ),
+                html.Div(
+                    id="content-2",
+                    children = [
+                        html.H2("Je suis le Cotent 2"),
+                        html.Div([
+                            dcc.Graph(id='comparison-graph')
+                        ], className='container mt-5'),
+                        html.P("""Je Suis le Deuxieme Content Super Interessante, Genre c'est 
+                               improbable personne me lie""")
+                    ]
                 )
             ]
         )
@@ -58,3 +74,24 @@ layout = html.Div(
 )
 def update_film(n_clicks, selected_films):
     return update_film_graph(n_clicks, selected_films, app.df)
+
+@callback(
+    Output('comparison-graph', 'figure'),
+    [Input('film-graph-content', 'hoverData')]
+)
+def update_comparison(hover_data):
+    return update_main_graph(hover_data, app.df)
+
+@callback(
+    Output('critic-graph', 'figure'),
+    [Input('update-film-button', 'n_clicks')]
+)
+def update_critic_graph_callback(n_clicks):
+    return update_critic_graph(app.df)
+
+@callback(
+    Output('pie-chart-content', 'figure'),
+    [Input('pie-chart-content', 'id')]
+)
+def display_pie_chart(id):
+    return create_pie_chart(app.df)
